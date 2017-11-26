e869765ae3bb1d893dd26605924b6bb4eef2d3e9
Bart Jeukendrup <bart@jeukendrup.nl>
2017-11-26 12:43:25 +0200

    hook into elgg logout

--- src/js/core/Logout.jsx
+++ src/js/core/Logout.jsx
@@ -1,42 +1,15 @@
 import React from "react"
-import { graphql } from "react-apollo"
-import gql from "graphql-tag"
 
-class Logout extends React.Component {
+export default class Logout extends React.Component {
     constructor(props) {
         super(props)
     }
 
-    componentDidMount() {
-        this.props.mutate({
-            variables: {
-                input: {
-                    clientMutationId: 1
-                }
-            }
-        }).then(({data}) => {
-            if (data.logout.viewer.loggedIn === false) {
-                window.location.href = '/'
-            }
-        })
-    }
-
     render() {
+        window.location.href = "/action/logout"
+
         return (
-            <div></div>
+            <div />
         )
     }
 }
-
-const query = gql`
-    mutation logout($input: logoutInput!) {
-        logout(input: $input) {
-            viewer {
-                guid
-                loggedIn
-            }
-        }
-    }
-`
-const withQuery = graphql(query)
-export default withQuery(Logout)
