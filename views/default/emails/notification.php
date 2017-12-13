c01938b6f92eb4b638203ab9a20f0f35300cfc81
Bart Jeukendrup <bart@jeukendrup.nl>
2017-12-13 11:18:55 +0100

    make links in html notifications clickable

--- views/default/emails/notification.php
+++ views/default/emails/notification.php
@@ -1,5 +1,8 @@
 <?php
 $body = elgg_extract("body", $vars);
+
+// make links clickable
+$body = preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $body);
 ?>
 <!-- Article -->
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
