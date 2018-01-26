bd93544a117ef9345dde80bfc48f6774bea8cfee
Bart Jeukendrup <bart@jeukendrup.nl>
2018-01-26 09:21:52 +0100

    fixed NotFound, show link to Pleio for configuring external login

--- src/js/profile/Settings.jsx
+++ src/js/profile/Settings.jsx
@@ -9,6 +9,23 @@ class Settings extends React.Component {
     render() {
         const { entity } = this.props.data
 
+        if (window.__SETTINGS__['externalLogin']) {
+            return (
+                <Wrapper match={this.props.match}>
+                    <section className="section ___grey ___grow">
+                        <div className="container">
+                            <div className="card-profile">
+                                <h3 className="card-profile__title">Instellingen</h3>
+                                <p>Voor het wijzigen van je wachtwoord en e-mailadres ga je naar Pleio.</p>
+                                <a href="https://www.pleio.nl/dashboard" className="button ___large" target="_blank">Ga naar Pleio</a>
+                            </div>
+                        </div>
+                    </section>
+                </Wrapper>
+            )
+        }
+
+
         if (!entity || !entity.canEdit) {
             return (
                 <div></div>
