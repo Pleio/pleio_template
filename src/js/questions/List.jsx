19e1b86520aa80d1e3a2c9072b755553e1212724
Bart Jeukendrup <bart@jeukendrup.nl>
2018-01-28 10:41:40 +0100

    added 'add question' button

--- src/js/questions/List.jsx
+++ src/js/questions/List.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { graphql } from "react-apollo"
+import gql from "graphql-tag"
 import ContentHeader from "../core/components/ContentHeader"
 import ContentFilters from "../core/containers/ContentFilters"
 import QuestionList from "./containers/QuestionList"
@@ -18,17 +20,30 @@ class List extends React.Component {
     }
 
     render() {
+        const { viewer } = this.props.data
+
+        let add
+        if (viewer && viewer.canWriteToContainer) {
+            add = (
+                <Link to={`questions/add`} className="button ___large ___add ___stick">
+                    <span>Stel een vraag</span>
+                </Link>
+            )
+        }
+
         return (
             <div className="page-container">
                 <Document title="Vragen" />
                 <ContentHeader>
-                    <h3 className="main__title">
-                        Vragen
-                    </h3>
-                    <ContentFilters page="questions" onClickAdd={this.onClickAdd} onChange={this.onChangeFilter} value={this.state.tags}>
-                        <Link to="/questions/add" className="right-lg">
-                        </Link>
-                    </ContentFilters>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h3 className="main__title">Vragen</h3>
+                        </div>
+                        <div className="col-sm-6 end-sm">
+                            {add}
+                        </div>
+                    </div>
+                    <ContentFilters page="questions" onClickAdd={this.onClickAdd} onChange={this.onChangeFilter} value={this.state.tags} />
                 </ContentHeader>
                 <section className="section ___grey ___grow">
                     <QuestionList childClass={Card} subtype="question" offset={0} limit={20} tags={this.state.tags} />
@@ -38,4 +53,14 @@ class List extends React.Component {
     }
 }
 
-export default List
+const Query = gql`
+    query QuestionsList {
+        viewer {
+            guid
+            loggedIn
+            canWriteToContainer(type: object, subtype: "question")
+        }
+    }
+`
+
+export default graphql(Query)(List)
